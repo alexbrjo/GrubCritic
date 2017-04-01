03812c9fb3b1e85e314ae0b9bb5e8849660874e5
Alex Johnson <alexjo5000@gmail.com>
2017-04-01 14:20:52 -0400

    Fixing ignored api keys

---
+++ frontend/core/Keys.js
@@ -0,0 +1,8 @@
+/**
+ * To successfully build test and run the project you must register for your
+ * own Google Maps and Yelp Fusion Applications. Enter your keys below and do
+ * not push.
+ */
+GrubCriticApp.prototype.keys = {
+    "google_maps_key" : "MAP-KEY"
+};
